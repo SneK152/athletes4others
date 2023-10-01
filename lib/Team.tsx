8d5ec58e60b9c463394a6eb31a8115f6c0091be4
snek152 <kakanisnehil@gmail.com>
2023-10-01 16:50:13 -0700

    adding officers

--- lib/Team.tsx
+++ lib/Team.tsx
@@ -7,18 +7,21 @@ export default function Team() {
       <h1 className="font-sans text-white uppercase text-5xl text-center font-bold">
         Meet the Team
       </h1>
-      <div className="flex gap-5 md:gap-0 md:flex-row flex-col justify-center items-center py-5">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-7 gap-5 md:gap-5 place-items-center py-5">
         {[
-          ["President", "Lorem Ipsum"],
-          ["Vice President", "Lorem Ipsum"],
-          ["Secretary", "Lorem Ipsum"],
-          ["Treasurer", "Lorem Ipsum"],
+          ["President", "Harsha Kadiyala"],
+          ["VP of Operations", "Samir Mehra"],
+          ["VP of Outreach", "Jeffrey Gao"],
+          ["Treasurer", "Nina Pan"],
+          ["Secretary", "Kylie Liao"],
+          ["Director of Soccer", "Uzayr Zameer"],
+          ["Director of Basketball", "Eric Huang"],
         ].map((person, i) => (
           <div
             key={i}
             className="flex flex-col items-center justify-center w-full"
           >
-            <div className="relative aspect-square w-[70%]">
+            <div className="relative aspect-square w-full">
               <Image
                 src="/hero_image.png"
                 fill
